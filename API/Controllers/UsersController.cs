0087f4588f52c1f396bafc9cd1680ed23622a982
vjmurenko <vjmurenko@gmail.com>
2021-04-18 11:19:42 +0300

    Backend APi

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+	[ApiController]
+	[Route("api/[controller]")]
+	public class UsersController : ControllerBase
+	{
+		private readonly DataContext _dataContext;
+
+		public UsersController(DataContext dataContext)
+		{
+			_dataContext = dataContext;
+		}
+
+		[HttpGet]
+		public ActionResult<IEnumerable<AppUser>> GetUserList()
+		{
+			return _dataContext.Users.ToList();
+		}
+
+
+		[HttpGet]
+		[Route("{id}")]
+		public ActionResult<AppUser> GetUser(int id)
+		{
+			return _dataContext.Users.Find(id);
+		}
+	}
+}
---
+++ API/Startup.cs
@@ -0,0 +1,50 @@
+using API.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace API {
+	public class Startup
+    {
+        private readonly IConfiguration _config;
+
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
+
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<DataContext>(options =>
+            {
+                options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
+            });
+
+            services.AddControllers();
+           
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
