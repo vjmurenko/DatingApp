29a1b35455cfca2757fdb2d96f5f7fbd57836fa3
vjmurenko <vjmurenko@gmail.com>
2022-01-11 17:34:14 +0300

    Change connection for Azure

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -8,8 +8,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace API.Extensions
-{
+namespace API.Extensions {
 	public static class ApplicationServiceExtensions
 	{
 		public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
@@ -26,9 +25,6 @@ namespace API.Extensions
 				var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
 
 				string connStr;
-
-				// Depending on if in development or production, use either Heroku-provided
-				// connection string, or development connection string from env var.
 				if (env == "Development")
 				{
 					// Use connection string from file.
@@ -36,25 +32,9 @@ namespace API.Extensions
 				}
 				else
 				{
-					// Use connection string provided at runtime by Heroku.
-					var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
-
-					// Parse connection URL to connection string for Npgsql
-					connUrl = connUrl.Replace("postgres://", string.Empty);
-					var pgUserPass = connUrl.Split("@")[0];
-					var pgHostPortDb = connUrl.Split("@")[1];
-					var pgHostPort = pgHostPortDb.Split("/")[0];
-					var pgDb = pgHostPortDb.Split("/")[1];
-					var pgUser = pgUserPass.Split(":")[0];
-					var pgPass = pgUserPass.Split(":")[1];
-					var pgHost = pgHostPort.Split(":")[0];
-					var pgPort = pgHostPort.Split(":")[1];
-
-					connStr = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb}";
+					connStr = config.GetSection("DefaultConnection").Value;
 				}
 
-				// Whether the connection string came from the local development configuration file
-				// or from the environment variable from Heroku, use it to set up your DbContext.
 				options.UseNpgsql(connStr);
 			});
 
